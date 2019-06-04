ad1d7629f275e5f8c5b884a4a35fcabba7c9d466
plenilune <alsrb1973@naver.com>
2019-06-05 08:33:39 +0900

    test

--- shop/urls.py
+++ shop/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('', product_in_category, name='product_all'),
     path('<slug:category_slug>/', product_in_category, name='product_in_category'),
     path('<int:id>/<product_slug>', product_detail, name='product_detail'),
+    path('upload/', ProductUploadView.as_view(), name='product_upload')
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 # Create your views here.
+from django.views.generic import CreateView, DeleteView, UpdateView
+
+from cart.cart import Cart
 from cart.forms import AddProductForm
 from .models import *
 
 
 def product_in_category(request, category_slug=None):
+    cart = Cart(request)
     current_category = None
     categories = Category.objects.all()
     products = Product.objects.filter(available_display=True)
@@ -16,6 +20,7 @@ def product_in_category(request, category_slug=None):
 
     return render(request, 'shop/list.html', {'current_category': current_category,
                                               'categories': categories,
+                                              'cart': cart,
                                               'products': products})
 
 
@@ -25,7 +30,37 @@ def product_detail(request, id, product_slug=None):
 
     return render(request, 'shop/detail.html', {'product': product, 'add_to_cart': add_to_cart})
 
+
 def product_write(request):
     return render(request, 'shop/write.html')
+
+
+class ProductUploadView(CreateView):
+    model = Product
+    #fields = Category.objects.all()
+    fields = ['category', 'name', 'slug', 'image', 'description', 'meta_description',
+              'price', 'stock', 'available_display', 'available_order']
+    template_name = 'shop/upload.html'
+
+    def form_valid(self, form):
+        form.instance.author_id = self.request.user.id
+        if form.is_valid():
+            form.instance.save()
+            return redirect('/')
+        else:
+            return self.render_to_response({'form': form})
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    success_url = '/'
+    template_name = 'shop/delete.html'
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    fields = ['category', 'name', 'slug', 'image', 'description', 'meta_description',
+              'price', 'stock', 'available_display', 'available_order']
+    template_name = 'shop/update.html'
 
 
